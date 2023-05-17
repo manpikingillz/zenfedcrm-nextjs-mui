f8a7eb45407fda7aee6aa86456c6389b268e1f7d
Gilbert Twesigomwe <gilbert@bposeats.com>
2023-05-17 06:17:42 +0300

    update title to ZenFed CRM

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -121,7 +121,7 @@ const App = (props: ExtendedAppProps) => {
     
       <CacheProvider value={emotionCache}>
         <Head>
-          <title>{`${themeConfig.templateName} - Material Design React Admin Template`}</title>
+          <title>{`${themeConfig.templateName}`}</title>
           <meta
             name='description'
             content={`${themeConfig.templateName} – Material Design React Admin Dashboard Template – is the most developer friendly & highly customizable Admin Dashboard Template based on MUI v5.`}
